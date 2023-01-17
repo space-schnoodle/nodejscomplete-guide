b0009edfb1568a46364879b38d28de0f0518225c
space-schnoodle <space.schnoodle@gmail.com>
2023-01-17 08:15:15 -0500

    working with template engines

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello world</h1>");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
